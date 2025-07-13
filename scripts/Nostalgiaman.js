131c74e54585d535e73704320236687a8564e212
Is an English Major <Najeebihesa@gmail.com>
2025-07-13 15:27:45 +0300

    added letter event listeners, added show word function

--- scripts/Nostalgiaman.js
+++ scripts/Nostalgiaman.js
@@ -1,21 +1,46 @@
 /*-------------------------------- Constants --------------------------------*/
-
-
-
-
+const letters = document.querySelectorAll(".letter")
+const messageElm = document.querySelector('.message')
+const word = ['ps2']
 /*---------------------------- Variables (state) ----------------------------*/
 
 
-
 /*------------------------ Cached Element References ------------------------*/
-
+// grab all of your buttons
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+// handle click function to push the letters to your word array (console.log the event.target.id)
+function init (){
+
+}
 
 
+//word check function 
+letters.forEach((oneLetter)=>{
+    oneLetter.addEventListener("click",(event)=>{
+        const pickedLetter = event.target.id.toLowerCase()
+        const splitWord = word[0].split("")
+        console.log(pickedLetter)
+        showWord(pickedLetter)
+        console.log(splitWord)
+        console.log(splitWord.includes(pickedLetter))
+    })
+})
 
 
+function showWord (pickedLetter){
+    console.log('Inside show word')
+    if (pickedLetter){
+        console.log('if statement')
+        messageElm.classList.remove('hidden')
+        messageElm.textContent = pickedLetter
+    }
+
+}
+init()
+showWord()
 /*----------------------------- Event Listeners -----------------------------*/
-
+// add event listener to all your buttons
+document.addEventListener('DOMContentLoaded', init)
