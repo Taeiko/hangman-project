8b289709169a5e94dbb7477dad917e4ad97d22b1
Is an English Major <Najeebihesa@gmail.com>
2025-07-14 14:43:29 +0300

    new playgame function

--- scripts/Nostalgiaman.js
+++ scripts/Nostalgiaman.js
@@ -7,7 +7,7 @@ const maxAttempts = 6
 let displayedWord = []
 let selectedWord = ""
 let pickedLetter;
-let remainingLives = []
+let wrongLetter = []
 /*------------------------ Cached Element References ------------------------*/
 // grab all of your buttons
 
@@ -16,67 +16,59 @@ let remainingLives = []
 /*-------------------------------- Functions --------------------------------*/
 // handle click function to push the letters to your word array (console.log the event.target.id)
 function init() {
-
+    currentWord = word[0]
 }
-function handleGuess(letters) {
-    if (correctGuess.includes(letter)) {
 
-    }
 
+function playGame() {
+
+    displayedWord = []
+    for (let i = 0; i < selectedWord.length; i++) {
+        displayedWord.push('_')
+
+    } console.log(displayedWord)
 
 }
 //letter choice function
 letters.forEach((oneLetter) => {
     oneLetter.addEventListener("click", (event) => {
         pickedLetter = event.target.id.toLowerCase()
-        showWord(pickedLetter)
+        if (currentWord.includes(pickedLetter)) {
+            showWord(pickedLetter)
+        }
     })
 })
 
 // shows blank spaces for the letters to go into 
-displayedWord = []
-for (let i = 0; i < selectedWord.length; i++) {
-    if (selectedWord[i] === ' ') {
-        displayedWord.push(' ')
-    } else {
-        displayedWord.push('_')
-    }
-}
 
 //show correct letter in the screen 
 function showWord(pickedLetter) {
     if (pickedLetter) {
         messageElm.classList.remove('hidden')
         messageElm.textContent += pickedLetter
-    }
-    selectedWord.split('').forEach((letter, idx) => {
+    } selectedWord.split('').forEach((letter, idx) => {
         let correctGuess = false
         if (letter === pickedLetter) {
             displayedWord[idx] = pickedLetter
             correctGuess = true
-
-        } //if the guess is correct 
-        if (correctGuess) {
-            document.querySelector('.word-disp').textContent = displayedWord.join(' ')
-            //if not correct..
-        } else {
-            remainingLives.includes(pickedLetter)
-            remainingLives.push(pickedLetters)
         }
     })
 }
-
 //to check if the letter isnt in the word - will take a point from available lives 
 function decductLife() {
 
 }
 
-function gameOver(letters) {
-  
+// Play game function
+// Pick the word to play with from words array
+// Add underscores to displayedWord besed on the length of chosen word
+// Display the displayed word array in the messageElem
+
+function gameOver() {
+
 }
 
 
-init()
 showWord(pickedLetter)
 /*----------------------------- Event Listeners -----------------------------*/
 // add event listener to all your buttons
