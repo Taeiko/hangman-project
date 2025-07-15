4afb854fbbf5e2dfe8ecafdde8b181a44b59bd42
Is an English Major <Najeebihesa@gmail.com>
2025-07-15 14:00:17 +0300

    added multiple functions (shoUnderscores, lifepoints, gameover, gamewin)

--- scripts/Nostalgiaman.js
+++ scripts/Nostalgiaman.js
@@ -2,13 +2,16 @@
 const letters = document.querySelectorAll(".letter")
 const messageElm = document.querySelector('.message')
 const wordUnderscores = document.querySelector("#word-undercores")
+const livesElement = document.querySelector(".lives")
+const gameOverContainer = document.querySelector("#game-over")
+const gameWinCont = document.querySelector("#game-win")
 const word = ['pstwo', 'gumball', 'sanandreas', 'gameboy', 'pokemon', 'tamagotchi']
-const maxAttempts = 6
 /*---------------------------- Variables (state) ----------------------------*/
 let displayedWord = []
 let selectedWord = ""
 let pickedLetter;
 let wrongLetter = []
+let lives = 6
 /*------------------------ Cached Element References ------------------------*/
 // grab all of your buttons
 
@@ -19,26 +22,11 @@ let wrongLetter = []
 function init() {
     pickRandomWord()
     playGame()
-    showUnderscores()
-    showWord(pickedLetter)
+    console.log(selectedWord)
 }
 
 
-// Play game function
-// Pick the word to play with from words array
-// Add underscores to displayedWord besed on the length of chosen word
-// Display the displayed word array in the messageElem
 
-
-
-// 1. display the _ on the page
-// 2. when the user clicks a letter check if the letter is in the word (if statement)
-//      a. add an event listener to each letter
-//      b. anytime the letter is pressed console.log() the letter value
-//      c. check if the letter they picked is in the displayWord array
-//      d. if its not than subtract 1 from the life variable
-//      e. if it is than show the letter on the page (This step has sub steps)
-// 3. deduct points based on wrong answer
 
 
 function playGame() {
@@ -52,7 +40,8 @@ function playGame() {
     }
     console.log(displayedWord)
     console.log(wordUnderscores)
-    }
+    livesElement.textContent = lives
+}
 
 
 //picks random word from word array 
@@ -62,14 +51,14 @@ function pickRandomWord() {
 
 
 //letter choice 
+//this only shows corrct guesses 
 letters.forEach((oneLetter) => {
     oneLetter.addEventListener("click", (event) => {
         pickedLetter = event.target.id.toLowerCase()
         if (selectedWord.includes(pickedLetter)) {
             showWord(pickedLetter)
-        }
+        } else {lifePoints()}
     })
-
 })
 
 // shows blank spaces for the letters to go into 
@@ -77,10 +66,11 @@ function showUnderscores() {
     for (i = 0; i < word.length; i++)
         messageElm.classList.remove('hidden')
     word.push('_')
-    for (i = 0; i < displayedWord.length; i++ )
-        pElement = document.createElement("p")
-        pElement.textContent = "_"
-        wordUnderscores.appendChild(pElement)
+
+    pElement = document.createElement("p")
+    pElement.textContent = "_"
+    wordUnderscores.appendChild(pElement)
+    console.log(pElement)
 }
 
 
@@ -88,30 +78,79 @@ function showUnderscores() {
 //show correct letter in the screen 
 //show correct letter in the screen 
 function showWord(pickedLetter) {
+    wordUnderscores.innerHTML = ""
     selectedWord.split('').forEach((letter, idx) => {
+
+
         if (letter === pickedLetter) {
-            displayedWord[idx] = pickedLetter;
-            // Replace the underscore with the guessed letter
-            const slot = wordUnderscores.children[idx];
-            if (slot) {
-                slot.textContent = pickedLetter;
-            }
+            displayedWord[idx] = letter
+
         }
+
+
     });
+    displayedWord.forEach((element) => {
+        let pElement = document.createElement("p")
+        pElement.textContent = element
+        wordUnderscores.appendChild(pElement)
+
+    })
+
+
+    console.log(displayedWord)
+    GameWin()
 }
 
-function checkLetter(keyboard){
-    keyboard.addEventListener()
 
-}
+
 //to check if the letter isnt in the word - will take a point from available lives 
-function decductLife() {
+function lifePoints() {
+    lives-=1
+    console.log(lives)
 
-}
+    livesElement.textContent = `Reamining lives ${lives}`
+
+    gameOver()
+} 
 
 function gameOver() {
+if (lives == 0){
+    console.log("Game Over")
+    let gameOverText = document.createElement('p')
+    gameOverText.textContent = 'Game over!'
+    gameOverContainer.appendChild(gameOverText)
+
+}
+}
+
+function GameWin (noUnderscores){
+    // loop through the entire displayWords
+    let noDashes = true
+
+
+    let hasWon = displayedWord.every((element)=>{
+        return element !== "_"
+    })
+
+    
+
+    if(hasWon){
+        console.log("YOU WIN")
+        let gameWinText = document.createElement('p')
+        gameWinText.textContent = "You won!"
+        gameWinCont.appendChild(gameWinText)
+    }
+
 
 }
 /*----------------------------- Event Listeners -----------------------------*/
 // add event listener to all your buttons
 document.addEventListener('DOMContentLoaded', init)
+
+
+
+
+
+
+// if the lives hit 0 instead of console.log show the game over on the page
+// winning and losing
